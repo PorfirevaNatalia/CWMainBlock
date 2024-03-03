02770d580a2ef75be51b0c362feb463001b50077
PorfirevaNatalia <miledydara@gmail.com>
2024-03-03 19:35:54 +0300

    В 3-м варианте первоначальный массив вводися пользователем с клавиатуры в одну строку

---
+++ 2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите элементы массива через пробел:");
+        string input = Console.ReadLine();
+        string[] inputArray = input.Split(' ');
+
+        string[] resultArray = GetStringsLengthThreeSymbols(inputArray);
+
+        Console.WriteLine("Результат:");
+        Console.WriteLine(string.Join(", ", resultArray));
+    }
+
+    static string[] GetStringsLengthThreeSymbols(string[] inputArray)
+    {
+        int count = 0;
+        for (int i = 0; i < inputArray.Length; i++)
+        {
+            if (inputArray[i].Length <= 3)
+            {
+                count++;
+            }
+        }
+
+        string[] resultArray = new string[count];
+        int index = 0;
+        for (int i = 0; i < inputArray.Length; i++)
+        {
+            if (inputArray[i].Length <= 3)
+            {
+                resultArray[index] = inputArray[i];
+                index++;
+            }
+        }
+        return resultArray;
+    }
+}
